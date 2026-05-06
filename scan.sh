61962f795fbc017f27ea878ac0d24640bae09b61
hackingyseguridad <antonio_taboada@hotmail.es>
2026-05-06 17:37:49 +0200

    Create scan.sh

---
+++ scan.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Esaneo de puertos/servicios y vulnerabilidades conocidas. 
+cat << "INFO"
+
+(:`--..___...-''``-._             |`._
+  ```--...--.      . `-..__      .`/ _\  
+            `\     '       ```--`.    />
+            : :   :               `:`-'
+             `.:.  `.._--...___     ``--...__      
+                ``--..,)       ```----....__,)   http://www.hackingyseguridad.com
+
+INFO
+if [ -z "$1" ]; then
+        echo
+        echo "Esaneo de puertos/servicios y vulnerabilidades conocidas."
+        echo "Requiere nmap"
+        echo "Uso.: sh scan.sh <ip/fqdn/rango>"
+        echo
+        exit 0
+fi
+echo
+echo
+nmap -Pn $1 $2 $3 $4 $5 -sVC -O --script=default,banner,vuln,vulners --script-args mincvss=7 -p- --open
