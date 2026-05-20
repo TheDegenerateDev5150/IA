1d43eaa48ba0446ea44271f11ec0c20dfa7357d8
hackingyseguridad <antonio_taboada@hotmail.es>
2026-05-20 13:07:48 +0200

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -21,9 +21,19 @@ if command -v systemctl >/dev/null 2>&1; then
     fi
 fi
 
-pipx install open-interpreter
-interpreter --local
-
+echo
+echo "... actualizando diccionarios ...  (R) 2025 hackingyseguridad.com "
+echo
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/refs/heads/master/ficheros.txt -q -O diccionario.txt  --inet4-only
+wc -l diccionario.txt
+echo ".."
 echo "..."
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/refs/heads/master/ficheros2.txt -q -O diccionario2.txt  --inet4-only
+wc -l diccionario2.txt
+echo "...."
+echo "....."
+echo
+echo "actualizado !!! "
+echo 
 
 
