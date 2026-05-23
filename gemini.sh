4eca6497227f2cc2b2efdb28e83445ff653e7a2b
hackingyseguridad <antonio_taboada@hotmail.es>
2026-05-23 16:16:13 +0200

    Update gemini.sh

--- gemini.sh
+++ gemini.sh
@@ -1,5 +1,17 @@
-
+#!/bin/sh
+# Instalar Gemini-Cli en Kali Linux , integrado con las interfaces y herramientas de Hacking del SO Kali
 # https://www.kali.org/tools/gemini-cli/
+cat << "INFO"
+
+(:`--..___...-''``-._             |`._
+  ```--...--.      . `-..__      .`/ _\  
+            `\     '       ```--`.    />
+            : :   :               `:`-'
+             `.:.  `.._--...___     ``--...__      
+                ``--..,)       ```----....__,)   http://www.hackingyseguridad.com
+                 Aldea del Fresno 2026
+$$$$$$ $$$$$ $$$$  $$$ $$$$  $$$     $$$$     $$$$$$ $$$$$ $$$$$$$$$ $$$$$$$$$ $$$
+INFO
 
 sudo apt install gemini-cli
 
