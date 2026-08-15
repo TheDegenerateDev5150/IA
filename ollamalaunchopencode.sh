83df407c37d54b227ffd22926459e70287d2bc71
hackingyseguridad <antonio_taboada@hotmail.es>
2026-08-16 00:17:07 +0200

    Create ollamalaunchopencode.sh

---
+++ ollamalaunchopencode.sh
@@ -0,0 +1,7 @@
+
+
+# integracion de modelos de ollama para poder usarlose en opencode
+# los modelos que tengamos descargados e instalados en ollama , los podemos usar en opencode
+
+ollama launch opencode
+
