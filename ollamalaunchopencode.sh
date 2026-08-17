7a858653abcc8188951675bb976d2f75b7ec240c
hackingyseguridad <antonio_taboada@hotmail.es>
2026-08-17 21:29:30 +0200

    Enhance script with author and usage comments
    Added author information and comments to the script.

--- ollamalaunchopencode.sh
+++ ollamalaunchopencode.sh
@@ -1,7 +1,13 @@
 
 
+
+# Antonio Taboada - hackingyseguridad.com 2026
+
 # integracion de modelos de ollama para poder usarlose en opencode
 # los modelos que tengamos descargados e instalados en ollama , los podemos ver, selecioar y usar en opencode
+# por ejemplo si hemos instalado previamente:  ollama run deepseek-r1:1.5b
+# podremos selecionar y ejecutar con opencode  -> deepseek-r1:1.5b, ideal para trabjar en modo local, sin internet!!! privacidad 
+
 
 ollama launch opencode
 
