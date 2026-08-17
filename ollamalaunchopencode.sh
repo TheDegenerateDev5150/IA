bb63362e9044dee8df68492fc91af8b42c89e9da
hackingyseguridad <antonio_taboada@hotmail.es>
2026-08-17 21:27:11 +0200

    Fix typo in comment about Ollama models usage

--- ollamalaunchopencode.sh
+++ ollamalaunchopencode.sh
@@ -1,7 +1,7 @@
 
 
 # integracion de modelos de ollama para poder usarlose en opencode
-# los modelos que tengamos descargados e instalados en ollama , los podemos usar en opencode
+# los modelos que tengamos descargados e instalados en ollama , los podemos ver, selecioar y usar en opencode
 
 ollama launch opencode
 
