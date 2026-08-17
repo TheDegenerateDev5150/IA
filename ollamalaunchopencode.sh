99b536c9a9065aa50adf5cd1ef1d2d5a71bbfc58
hackingyseguridad <antonio_taboada@hotmail.es>
2026-08-17 21:36:43 +0200

    Include example command for deepseek-r1:1.5b
    Add example command for launching a specific model.

--- ollamalaunchopencode.sh
+++ ollamalaunchopencode.sh
@@ -5,7 +5,7 @@
 # los modelos que tengamos descargados e instalados en ollama , los podemos ver, selecioar y usar en opencode
 # por ejemplo si hemos instalado previamente:  ollama run deepseek-r1:1.5b
 # podremos selecionar y ejecutar con opencode  -> deepseek-r1:1.5b, ideal para trabjar en modo local, sin internet!!! privacidad 
-
+# ollama launch opencode --model deepseek-r1:1.5b
 
 ollama launch opencode
 
