0a31bf12defcf2a392ee91e2f8f16de8f1d0549f
hackingyseguridad <antonio_taboada@hotmail.es>
2026-09-03 11:16:57 +0200

    Create chrome.sh

---
+++ chrome.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# (r) hacking y seguridad .com 2026 Version Beta - Antonio Taboada 
+echo "###############################################################"
+echo "Ejecutando el navegador google Chrome, en Linux ... "
+echo "###############################################################"
+echo
+google-chrome-stable --no-sandbox --user-data-dir
