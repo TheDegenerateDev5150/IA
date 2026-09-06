4091f7187e58365f75c7c98262d06e7088700a38
hackingyseguridad <antonio_taboada@hotmail.es>
2026-09-06 17:13:57 +0200

    Create offline.sh

---
+++ offline.sh
@@ -0,0 +1,13 @@
+
+
+#!/bin/sh
+# IA en Local sin GPU offline !
+# @antonio_taboada - hackingyseguridad.com - 2026
+
+echo " "
+echo "IA en Local sin GPU !"
+echo "ejecuta deepseek-r1:1.5b de ollama en LOCAL offline, con opencode "
+echo " "
+
+ollama launch opencode --model deepseek-r1:1.5b
+
