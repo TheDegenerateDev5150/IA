08bc3554800eab33be72f43040b2127fc332fabf
hackingyseguridad <antonio_taboada@hotmail.es>
2026-09-06 17:21:31 +0200

    Update offline.sh

--- offline.sh
+++ offline.sh
@@ -9,5 +9,11 @@ echo "IA en Local sin GPU !"
 echo "ejecuta deepseek-r1:1.5b de ollama en LOCAL offline, con opencode "
 echo " "
 
+# otros modelos similares en LOCAL y sin GPU !! solo CPU
+#
+# ollama launch opencode --model monotykamary/whiterabbitneo-v1.5a 
+# ollama launch opencode --model captainkyd/whiterabbitneo7b
+# ollama launch opencode --model lazarevtill/WhiteRabbitNeo-2.5-Qwen-2.5-Coder-7B
+#
 ollama launch opencode --model deepseek-r1:1.5b
 
