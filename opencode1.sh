8c35a1e1c3cd5ec5c933b3d595f412504465a24b
hackingyseguridad <antonio_taboada@hotmail.es>
2026-09-06 18:19:28 +0200

    Create opencode1.sh

---
+++ opencode1.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# IA conmbinando OpenCODE y OLLAMA !
+# @antonio_taboada - hackingyseguridad.com - 2026
+
+echo " "
+echo "IA combianndo Opencode y Ollama !"
+echo "ejecuta modelos online de ollama con opencode "
+echo " "
+
+ollama launch opencode --model gemma4:31b-cloud
+
+# otros modelos similares
+#
+# ollama launch opencode --model minimax-m3:cloud
+# ollama launch opencode --model gpt-oss:20b-cloud
